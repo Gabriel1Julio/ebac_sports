5a553904c8b3001c16509c39dd851a59dd587f33
Gabriel Julio ALves de Souza <gj86225@gmail.com>
2024-12-05 16:43:23 -0300

    aplicando as mudanças com o Redux no código

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,19 @@
 import * as S from './styles'
 
-import { Produto } from '../../App'
-
 import cesta from '../../assets/cesta.png'
 import { paraReal } from '../Produto'
 
-type Props = {
-  itensNoCarrinho: Produto[]
-  favoritos: Produto[]
-}
+import { RootReducer } from '../../store'
+import { useSelector } from 'react-redux'
+import { Produto } from '../../App'
 
-const Header = ({ itensNoCarrinho, favoritos }: Props) => {
-  const valorTotal = itensNoCarrinho.reduce((acc, item) => {
+const Header = () => {
+  const itens = useSelector(
+    (state: RootReducer) => state.app.carrinho as Produto[]
+  )
+  const favoritos = useSelector((state: RootReducer) => state.app.favoritos)
+
+  const valorTotal = itens.reduce((acc, item) => {
     acc += item.preco
     return acc
   }, 0)
@@ -23,7 +25,7 @@ const Header = ({ itensNoCarrinho, favoritos }: Props) => {
         <span>{favoritos.length} favoritos</span>
         <img src={cesta} />
         <span>
-          {itensNoCarrinho.length} itens, valor total: {paraReal(valorTotal)}
+          {itens.length} itens, valor total: {paraReal(valorTotal)}
         </span>
       </div>
     </S.Header>
--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,42 +1,42 @@
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
+import { useGetProdutosQuery } from '../services/api'
 
 import * as S from './styles'
 
-type Props = {
-  produtos: ProdutoType[]
-  favoritos: ProdutoType[]
-  adicionarAoCarrinho: (produto: ProdutoType) => void
-  favoritar: (produto: ProdutoType) => void
-}
+import { useSelector, useDispatch } from 'react-redux'
+import { favoritar } from '../store/reducers/carrinho'
+import { RootReducer } from '../store'
 
-const ProdutosComponent = ({
-  produtos,
-  favoritos,
-  adicionarAoCarrinho,
-  favoritar
-}: Props) => {
+const ProdutosComponent = () => {
+  const { data: produtos, isLoading } = useGetProdutosQuery()
+
+  const favoritos = useSelector(
+    (state: RootReducer) => state.app.favoritos as ProdutoType[]
+  )
+  const dispatch = useDispatch()
+
+  if (isLoading) return <h2>Carregando...</h2>
+
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
-    const produtoId = produto.id
-    const IdsDosFavoritos = favoritos.map((f) => f.id)
+    return favoritos.some((favorito) => favorito.id === produto.id)
+  }
 
-    return IdsDosFavoritos.includes(produtoId)
+  const handleFavoritar = (produto: ProdutoType) => {
+    dispatch(favoritar(produto))
   }
 
   return (
-    <>
-      <S.Produtos>
-        {produtos.map((produto) => (
-          <Produto
-            estaNosFavoritos={produtoEstaNosFavoritos(produto)}
-            key={produto.id}
-            produto={produto}
-            favoritar={favoritar}
-            aoComprar={adicionarAoCarrinho}
-          />
-        ))}
-      </S.Produtos>
-    </>
+    <S.Produtos>
+      {produtos?.map((produto) => (
+        <Produto
+          estaNosFavoritos={produtoEstaNosFavoritos(produto)}
+          key={produto.id}
+          produto={produto}
+          favoritar={handleFavoritar}
+        />
+      ))}
+    </S.Produtos>
   )
 }
 
